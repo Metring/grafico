3625f96d94c6d8dedd8a546b783b8b318f2d43aa
Metring <127319012+Metring@users.noreply.github.com>
2024-10-02 09:24:30 -0300

    c

---
+++ main.js
@@ -0,0 +1,23 @@
+const url =  "https://servicodados.ibge.gov.br/api/v3/agregados/1008/periodos/2006/variaveis/2020?localidades=N1[all]%7CN3[41,42,43]&classificacao=12596[118149]%7C12591[0]";
+
+ 
+  
+
+async function visualizarDados(){
+
+  const res = await fetch(url)
+
+  const dados = await res.json()
+ console.log(dados)
+  regiao = 1
+
+   resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
+  
+
+  resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
+
+  console.log(resultado_dados, resultado_regiao)
+
+}
+
+visualizarDados()
